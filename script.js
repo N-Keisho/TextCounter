f295221d1dd4d949fd55647456c2aae985c2d702
KEI$HO <133760530+N-Keisho@users.noreply.github.com>
2024-06-02 23:50:42 +0900

    改行文字をカウント除外

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const textBox = document.getElementById("input");
 const count = document.getElementById("count");
 
 textBox.addEventListener("input", () => {
-    count.innerText = textBox.value.length;
+    let text = textBox.value;
+    text = text.replace(/\s| /g, "");
+    text = text.replace('\n', "");
+    count.innerText = text.length;
 });
